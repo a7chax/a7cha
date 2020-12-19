fa8a7c3905708ec384288887dcf2be9628fc18ea
a7cha <akuarung83@gmail.com>
2020-12-19 19:24:55 +0700

    first commit

---
+++ addingswap.sh
@@ -0,0 +1,9 @@
+sudo fallocate -l 1G /swapfile && 
+sudo chmod 600 /swapfile && 
+sudo mkswap /swapfile&& 
+sudo swapon /swapfile &&
+sudo echo "/swapfile swap swap defaults 0 0" >> /etc/fstab &&
+sudo swapon --show &&
+sudo free -h && 
+sudo echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf &&
+sudo echo "vm.swappiness = 90" >> /etc/sysctl.conf 
