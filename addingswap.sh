27262b18b1fcd61ab2fde6022d8445eed26a7700
Arungi Cahaya <47318515+a7cha@users.noreply.github.com>
2020-12-19 19:46:40 +0700

    Update addingswap.sh

--- addingswap.sh
+++ addingswap.sh
@@ -1,4 +1,5 @@
 ## tested on ubuntu 20.04
+## adding 1GB SWAP if you wan more just replace 1G to 24G for 24GB or whatever
 sudo fallocate -l 1G /swapfile && 
 sudo chmod 600 /swapfile && 
 sudo mkswap /swapfile&& 
